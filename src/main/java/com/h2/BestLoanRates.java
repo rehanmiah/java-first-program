93f384c56cedbb72ff62a6f6eba8e595b004eef1
rehanmiah <rehan_miah@hotmail.com>
2021-02-13 19:58:07 +0000

    Update BestLoanRates.java

--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class BestLoanRates {
  public static void main(String[] args){
      var Scanner = new Scanner(System.in);
-     System.out.println("Enter you name");
+     System.out.println("Enter your name");
      var name = Scanner.nextLine();
      System.out.println("hello" + name);
  }
