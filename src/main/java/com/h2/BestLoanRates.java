d4db77354072715818c7338971af3c43d496ac8f
rehanmiah <rehan_miah@hotmail.com>
2021-02-13 19:59:38 +0000

    Update BestLoanRates.java

--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -6,6 +6,6 @@ public class BestLoanRates {
      var Scanner = new Scanner(System.in);
      System.out.println("Enter your name");
      var name = Scanner.nextLine();
-     System.out.println("Hello" + name);
+     System.out.println("Hello "+ name);
  }
 }
