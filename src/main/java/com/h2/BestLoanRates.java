eb1cffa8485fc8991cd7df67301a00606638de23
rehanmiah <rehan_miah@hotmail.com>
2021-02-13 19:10:27 +0000

    Create BestLoanRates.java

---
+++ src/main/java/com/h2/BestLoanRates.java
@@ -0,0 +1,12 @@
+package com.h2;
+
+public class BestLoanRates {
+ public static void main(String[] args){
+     var scanner;
+     new Scnner(System.in);
+     system.out.println("Enter you name");
+     var name;
+     scanner.nextLine();
+     system.out.println("hello" + name);
+ }
+}
