a2437bc518cf5387c0a964712b064322ec5d4485
rehanmiah <rehan_miah@hotmail.com>
2021-02-14 16:43:38 +0000

    Create savingsCalaculator.java

---
+++ src/test/java/com/h2/savingsCalaculator.java
@@ -0,0 +1,9 @@
+package com.h2;
+
+public class savingsCalaculator {
+    private static float credits(){};
+    private static float  debits(){};
+    public static void main(float credits){
+
+    }
+}
