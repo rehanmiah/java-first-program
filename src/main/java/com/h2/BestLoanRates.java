531461ffc52a5ff7b65ced8095372d25d9af65ee
rehanmiah <rehan_miah@hotmail.com>
2021-02-14 14:53:45 +0000

    Update BestLoanRates.java

--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -9,6 +9,10 @@ public class BestLoanRates {
      var name = Scanner.nextLine();
      System.out.println("Hello "+ name);
  }
+ public static float getRates(int[] args){
+     return 0.0f;
+
+ }
     public static final Map<Integer, Float> bestRates = Map.of(
             1, 5.50f,
             2, 3.45f,
