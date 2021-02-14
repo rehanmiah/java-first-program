3423ac556c07145d236c2ee4f8dd876238ba37af
rehanmiah <rehan_miah@hotmail.com>
2021-02-14 19:07:11 +0000

    Update SavingsCalculator.java

--- src/test/java/com/h2/SavingsCalculator.java
+++ src/test/java/com/h2/SavingsCalculator.java
@@ -39,7 +39,22 @@ public class SavingsCalculator {
     }
 
     public static void main(String[] args){
+        String[] creditsAsString = args[0].split(",");
+        String[] debitsAsString = args[1].split(",");
 
+        float credits = new float creditsAsString.length;
+        for( int i=0; i<creditsAsString.length; i++){
+            Float.parseFloat(creditsAsString[i])
+        }
+        float debits = new float debitsAsString.length;
+        for( int i=0; i<debitsAsString.length; i++){
+            Float.parseFloat(debitsAsString[i]);
+        }
+
+        final SavingsCalculator calculator = new SavingsCalculator(credits, debits);
+        float netSavings = calculator.calculate();
+        System.out.println("Net Savings = " + netSavings + ", remaining days in month = " + remainingDaysInMonth(LocalDate.now()));
     }
 }
+
 
