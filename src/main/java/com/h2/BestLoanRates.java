f612e67a4e1f35d0e6f079681c823e916d4680e0
rehanmiah <rehan_miah@hotmail.com>
2021-02-14 15:55:19 +0000

    Update BestLoanRates.java

--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -1,29 +1,36 @@
 
 package com.h2;
-import java.util.Scanner;
-import java.util.Map;
-public class BestLoanRates {
- public static void main(String[] args){
-     var Scanner = new Scanner(System.in);
-     System.out.println("Enter your name");
-     var name = Scanner.nextLine();
-     System.out.println("Hello "+ name);
-     final var bestRates = Map.of(1, 5.50f, 2, 3.45f, 3, 2.67f);
- }
-<<<<<<< HEAD
- public static float getRates(int args){
-     if (bestRates.containskey(loanTermInYears)){
-         return bestRates.get(loanTermInYears);
-     }
 
- }
+import java.util.Map;
+import java.util.Scanner;
+
+public class BestLoanRates {
     public static final Map<Integer, Float> bestRates = Map.of(
             1, 5.50f,
             2, 3.45f,
             3, 2.67f
     );
-}
-=======
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name = scanner.nextLine();
+        System.out.println("Hello " + name);
+
+        System.out.println("Enter the loan term (in years)");
+        int loanTermInYears = scanner.nextInt();
+        float bestRate = getRates(loanTermInYears);
+        if(bestRate == 0.0f) {
+            System.out.println("No available rates for term: " + loanTermInYears + " years");
+        } else {
+            System.out.println("Best Available Rate: " + bestRate + "%");
+        }
+    }
+
+    public static float getRates(int loanTermInYears) {
+        if(bestRates.containsKey(loanTermInYears)) {
+            return bestRates.get(loanTermInYears);
+        }
+        return 0.0f;
+    }
 }
->>>>>>> parent of db82652... Update BestLoanRates.java
