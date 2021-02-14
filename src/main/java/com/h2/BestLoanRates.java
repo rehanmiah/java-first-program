09aad2e852ac69f6fafeef6defaa35db74226c20
rehanmiah <rehan_miah@hotmail.com>
2021-02-14 15:51:44 +0000

    Update BestLoanRates.java

--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -3,5 +3,21 @@ package com.h2;
 import java.util.Scanner;
 import java.util.Map;
 public class BestLoanRates {
+ public static void main(String[] args){
+     var Scanner = new Scanner(System.in);
+     System.out.println("Enter your name");
+     var name = Scanner.nextLine();
+     System.out.println("Hello "+ name);
+ }
+ public static float getRates(int args){
+     if (bestRates.containskey(loanTermInYears)){
+         return bestRates.get(loanTermInYears);
+     }
 
+ }
+    public static final Map<Integer, Float> bestRates = Map.of(
+            1, 5.50f,
+            2, 3.45f,
+            3, 2.67f
+    );
 }
