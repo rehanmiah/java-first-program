af24d7b0bf8960352bac67634d7bea95ba8675a5
rehanmiah <rehan_miah@hotmail.com>
2021-02-14 16:45:30 +0000

    Update savingsCalaculator.java

--- src/test/java/com/h2/savingsCalaculator.java
+++ src/test/java/com/h2/savingsCalaculator.java
@@ -1,8 +1,12 @@
 package com.h2;
 
 public class savingsCalaculator {
-    private static float credits(){};
-    private static float  debits(){};
+    private static float credits(){
+        return 0;
+    };
+    private static float  debits(){
+        return 0;
+    };
     public static void main(float credits){
 
     }
